87ff4b906e6367a63403317c03949e30115fb800
Marwen Kheder <marwen.kheder@gmail.com>
2014-04-15 10:43:05 +0100

    Reservation coté Passager : Liste de reservation et nbr de reservation ...

--- src/site/reservationBundle/Controller/DefaultController.php
+++ src/site/reservationBundle/Controller/DefaultController.php
@@ -10,16 +10,34 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $nbvalide = 0;
         $em = $this->container->get('doctrine')->getEntityManager();
         $vols = $em->getRepository('siteadminBundle:vol')->FindAll();
         $csrfToken = $this->container->has('form.csrf_provider')
             ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
             : null;
 
+
+        $securityContext = $this->container->get('security.context');
+        if( $securityContext->isGranted('IS_AUTHENTICATED_FULLY') ){
+
+            $userid = $this->get('security.context')->getToken()->getUser()->getId();
+            $user = $em->find('siteadminBundle:Passager', $userid);
+            $repo = $this->getDoctrine()->getRepository('siteadminBundle:Reservation');
+            $nbvalide = count($repo->findBy(
+                array('etat'=> 'V','passager'=> $user)
+            ));
+        }
+
+
+
+
+
         return $this->render('sitereservationBundle:Default:index.html.twig',
         array(
             'vols'=>$vols,
-            'csrf_token'=>$csrfToken
+            'csrf_token'=>$csrfToken,
+            'nbvalide'=>$nbvalide
         ));
     }
     public function reserverAction($id = null){
@@ -29,6 +47,10 @@ class DefaultController extends Controller
         $userid = $this->get('security.context')->getToken()->getUser()->getId();
 
         $user = $em->find('siteadminBundle:Passager', $userid);
+
+
+
+
 
         $r = new Reservation();
         $r->setEtat('A');
@@ -40,6 +62,27 @@ class DefaultController extends Controller
 
 
        return $this->redirect($this->generateUrl('sitereservation_homepage'));
+
+    }
+
+    public function mesreservationAction(){
+        $em = $this->container->get('doctrine')->getEntityManager();
+
+        $r = $this->getDoctrine()->getRepository('siteadminBundle:Reservation');
+        $userid = $this->get('security.context')->getToken()->getUser()->getId();
+
+        $user = $em->find('siteadminBundle:Passager', $userid);
+
+        $nbvalide = count($r->findBy(
+            array('etat'=> 'V','passager'=> $user)
+        ));
+
+        $reservation = $r->findByPassager($user);
+        return $this->render('sitereservationBundle:Default:mesreservations.html.twig',
+            array(
+                'vols'=>$reservation,
+                'nbvalide'=>$nbvalide
+            ));
 
     }
 
